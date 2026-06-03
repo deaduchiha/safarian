78a4e2b29a0041a0287085e7862f88dded24ecb6
Alireza Nikzad <nikzad.alireza79@gmail.com>
2026-06-03 12:52:40 +0330

    Update session cookie configuration to extend maxAge to 7 days and set updateAge to 1 day for improved session management.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -58,7 +58,9 @@ export const auth = betterAuth({
     deferSessionRefresh: true,
     cookieCache: {
       enabled: true,
-      maxAge: 60 * 5,
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+      updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
+
     },
   },
   plugins: [username(), nextCookies()],
